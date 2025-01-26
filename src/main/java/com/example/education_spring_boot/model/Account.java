c01d59b72711a4586353e64e571826070773e88b
Quách Vinh Quang <hynoras1@gmail.com>
2025-01-26 16:50:01 +0700

    added Account model and repository

---
+++ src/main/java/com/example/education_spring_boot/model/Account.java
@@ -0,0 +1,31 @@
+package com.example.education_spring_boot.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Account")
+public class Account {
+    @Id
+    @GeneratedValue()
+    private String id;
+
+    @Column(name = "username", unique = true, nullable = true, length = 20)
+    private String username;
+
+    @Column(name = "password", unique = false, nullable = true)
+    private String password;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
---
+++ src/main/java/com/example/education_spring_boot/repository/admin/AccountRepo.java
@@ -0,0 +1,7 @@
+package com.example.education_spring_boot.repository.admin;
+
+import com.example.education_spring_boot.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepo extends JpaRepository<Account, Long> {
+}
