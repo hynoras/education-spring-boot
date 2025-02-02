0eb99d9924447694e83bad13b32ad266d10146bb
Quách Vinh Quang <hynoras1@gmail.com>
2025-02-02 13:56:57 +0700

    added spring security

--- src/main/java/com/example/education_spring_boot/config/SecurityConfig.java
+++ src/main/java/com/example/education_spring_boot/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.example.education_spring_boot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,9 +21,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth
-            .requestMatchers(HttpMethod.POST, "/api/*").permitAll()
-            .anyRequest().authenticated());
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, "/api/admin/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+            );
 
         return http.build();
     }
--- src/main/java/com/example/education_spring_boot/controller/admin/AccountController.java
+++ src/main/java/com/example/education_spring_boot/controller/admin/AccountController.java
@@ -3,8 +3,10 @@ package com.example.education_spring_boot.controller.admin;
 import com.example.education_spring_boot.dto.AccountDTO;
 import com.example.education_spring_boot.model.Account;
 import com.example.education_spring_boot.service.admin.Account.AccountService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,10 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
-    @PostMapping("/register")
-    public ResponseEntity<Account> register(@RequestBody AccountDTO accountDTO) {
-        Account account = accountService.register(accountDTO);
-        return ResponseEntity.ok(account);
-    }
+        @PostMapping("/register")
+        @PreAuthorize("hasAuthority('ADMIN')")
+        public ResponseEntity<Account> register(@Valid @RequestBody AccountDTO accountDTO) {
+            Account account = accountService.register(accountDTO);
+            return ResponseEntity.ok(account);
+        }
 }
