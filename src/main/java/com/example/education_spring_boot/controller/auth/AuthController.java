8d5a4bcc807f9eb1fb89e7620a9abc9890fcfb35
Quách Vinh Quang <hynoras1@gmail.com>
2025-02-20 10:08:52 +0700

    modify the response of /get-user to return 1 role instead of an array

--- src/main/java/com/example/education_spring_boot/controller/auth/AuthController.java
+++ src/main/java/com/example/education_spring_boot/controller/auth/AuthController.java
@@ -13,10 +13,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/auth")
@@ -55,9 +57,15 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not authenticated");
         }
 
+        Optional<String> roleOptional = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .findFirst();
+
+        String role = roleOptional.orElse("USER");
+
         return ResponseEntity.ok(Map.of(
                 "username", authentication.getName(),
-                "roles", authentication.getAuthorities()
+                "role", role
         ));
     }
 
